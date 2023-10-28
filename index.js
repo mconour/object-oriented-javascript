d70d8998ba1aac6de713366ffad432f20aac5a37
mconour <michaelconour@gmail.com>
2023-10-28 16:44:26 -0700

    create Book object

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
-class Person {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.job = "";
-    }
+/* 
+You can create two classes, Library and Book, where a library contains a collection of books. 
+The Library class can have methods for adding books, removing books, and listing all the books it contains. 
+The Book class can have properties like title, author, and publication year.
+*/
 
-    getName = () => this.name;
-    getAge = () => this.age;
-    getGender = () => this.gender;
-    setJob = (job) => this.job = job;
-}
-
-class House {
-    constructor(address, price, residents) {
-        this.address = address;
-        this.price = price;
-        this.residents = residents;
-    }
-
-    getAddress = () => this.address;
-    getPrice = () => this.price;
-    getResidents = () => this.residents;
-
-
-    addResident = (resident) => this.residents.push(resident);
-}
-
-class Programmer extends Person {
-    constructor(name, age, company, salary, language) {
-        super(name, age);
-        this.company = company;
-        this.salary = salary;
-        this.language = language;
-    }
-
-    sayHi = () => {
-        console.log(`Hello, I'm a programmer! My name is ${this.getName()} and I'm ${this.getAge()}. I work for ${this.company}`);
+class Book {
+    constructor(title, author, publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
     }
 }
 
-let programmer = new Programmer('Michael', 19, 'Twitch', 1000000, 'JavaScript');
-programmer.sayHi();
+class Library {
+    constructor(params) {
+        
+    }
+}
